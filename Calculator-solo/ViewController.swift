c510d346fdca25ec60f03e7c70f363401aad499e
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2023-04-24 23:30:07 +0900

    [ADD] tabNumberValue function defined

--- Calculator-solo/ViewController.swift
+++ Calculator-solo/ViewController.swift
@@ -7,14 +7,59 @@
 
 import UIKit
 
+enum Operation {
+    case Add
+    case Subtract
+    case Multiply
+    case Divide
+    case unknown
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var NumberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    var currentOperation: Operation = .unknown
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tabNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.titleLabel?.text else {
+            return
+        }
+        
+        if self.displayNumber.count < 9 { // maximum 9 significant figures can be entered
+            self.displayNumber += numberValue
+            self.NumberOutputLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tabClearButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabDotButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabDivideButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabMultiplyButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabAddButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabSubtractButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabEqualButton(_ sender: UIButton) {
+    }
 }
 
